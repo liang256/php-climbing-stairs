faaf144ead83aa56bcef7f5072a66d5417caceee
Leon Chen <>
2022-09-23 00:08:18 +0800

    style: add comments

--- src/Solution.php
+++ src/Solution.php
@@ -11,8 +11,11 @@ class Solution {
         } elseif ($n <= 1) {
             return 1;
         }
-        $prev1 = 1;
-        $prev2 = 1;
+        
+        $prev1 = 1; // f(0) = 1
+        $prev2 = 1; // f(1) = 1
+
+        // when n >= 2, f(n) = f(n-1) + f(n-2)
         for ($i = 2; $i <= $n; $i++) {
             $tmp = $prev1 + $prev2;
             $prev1 = $prev2;
